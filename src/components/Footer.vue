1204381c1bb2f298f6acdc45d503bc5301fdc047
eden <flyinfreedom1124@gmail.com>
2020-07-18 20:58:18 +0800

    resolve copyright by candy, and fix facebook url

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,7 @@
       <span class="address">龍潭區大平里永平路80號</span>
     </div>
     <div class="footer__wrapper">
-      <span class="copyright">Copyright © Candy All Rights Reserved.</span>
+      <span class="copyright">Copyright © 桃園市龍潭區石門社區發展協會 All Rights Reserved.</span>
       <ul class="link">
         <li v-for="link in links" :key="link.url">
           <a :href="link.url" target="_blank">{{ link.label }}</a>
@@ -32,7 +32,7 @@ export default class Travel extends Vue {
     },
     {
       label: 'FaceBook',
-      url: 'https://www.facebook.com',
+      url: 'https://www.facebook.com/h4713588/',
     }
   ];
 }
